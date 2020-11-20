d6df14377c3e7687e2bb802345ac70805ba0b796
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2020-11-20 16:28:39 -0300

    tipos de dados

---
+++ tipos de dados/script.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+var nome = 'Gustavo';
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var frase = 'romário fez 1 gol';
+console.log(frase);
+
+// Declare uma variável com a sua idade
+var idade = 21;
+console.log(idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var meuNome = 'Gustavo Felipe';
+var meuSobrenome = 'Batista Carneiro';
+var meuNomeCompleto = `${meuNome} ${meuSobrenome}`
+console.log(meuNomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase1 = "It's time";
+console.log(frase1);
+
+// Verifique o tipo da variável que contém o seu nome
+var verificarTipoNome = typeof meuNome;
+console.log(verificarTipoNome);
---
+++ tipos de dados/script_aula.js
@@ -0,0 +1,25 @@
+var nome = 'Gustavo';
+var idade = 21;
+var simbolo = Symbol();
+console.log(typeof simbolo);
+
+var nome = 'Gustavo';
+var sobrenome = 'Felipe'
+var nomeCompleto = nome + ' ' + sobrenome;
+console.log(nomeCompleto);
+
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + " gols"
+// console.log(frase);
+
+var ano = '2020';
+var mes = 8;
+console.log(ano + mes);
+
+var melhor = 'teste';
+var frase1 = "esse é o \"melhor\" jogo";
+console.log(frase1);
+
+var gols = 1000;
+var frase2 = `Romário fez ${gols * 2} gols`
+console.log(frase2);
