5f5af8b1b9a42dc2b01089db6d1f0086b732d0c7
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2020-11-20 15:55:51 -0300

    variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+var meuNome = 'Gustavo Felipe Batista Carneiro';
+console.log(meuNome);
+
+// Declarar uma variável com a sua idade
+var minhaIdade = 21;
+console.log(minhaIdade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaPreferida;
+console.log(comidaPreferida);
+
+// Atribuir valor a sua comida favorita
+comidaPreferida = 'Lasanha de pressão';
+console.log(comidaPreferida);
+// Declarar 5 variáveis diferentes sem valores
+var semValor1, semValor2, semValor3, semValor4, semValor5;
+console.log(semValor1, semValor2, semValor3, semValor4, semValor5);
---
+++ variaveis/script_aula.js
@@ -0,0 +1,31 @@
+var nome = 'Gustavo';
+var idade = 21;
+var possuiFaculdade = true;
+
+console.log(nome, idade, possuiFaculdade);
+
+var preco = 25;
+var totalComprato = 5;
+var totalPreco = preco * totalComprato;
+
+console.log(totalPreco);
+
+var sobrenome = 'Felipe',
+    cidade = 'Marabá';
+
+console.log(sobrenome, cidade);
+
+var semDefinir;
+
+console.log(semDefinir);
+
+// console.log(aindaNaoDefini)
+var comida;
+console.log(comida)
+comida = 'pizza'
+console.log(comida)
+
+// const time = 'vasco';
+// time ='flamengo'
+
+// console.log(time);
