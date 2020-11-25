e28dde3cb1c62768b0d698f265881bf24042f63f
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2020-11-25 16:04:03 -0300

    arrays e loops 1

---
+++ arrays e loops/script.js
@@ -0,0 +1,14 @@
+var videoGames = ['Switch', 'PS4', 'Xbox'];
+
+var ultimoItem = videoGames.pop();
+
+for(let numero = 1; numero <= 10; numero++){
+    console.log(numero);
+}
+
+var i = 0;
+
+while(i < 10){
+    console.log(i);
+    i++;
+}
