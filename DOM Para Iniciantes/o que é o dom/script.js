640ea9695e53be8ff41c30712b3acfce539b99e0
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2020-11-25 22:09:22 -0300

    o que e dom 1

---
+++ DOM Para Iniciantes/o que é o dom/script.js
@@ -0,0 +1,13 @@
+// window.alert('Isso mesmo');
+
+const href = "http://127.0.0.1:5500/DOM%20Para%20Iniciantes/o%20que%20%C3%A9%20o%20dom/";
+
+console.log(href);
+
+if(href === '"http://127.0.0.1:5500/DOM'){
+  console.log('é igual');
+}
+
+const h1Selecionado = document.querySelector('h1');
+
+alert('Teste');
