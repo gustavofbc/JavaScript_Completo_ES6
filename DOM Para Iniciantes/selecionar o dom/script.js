759f23d8ffbd3e2440cdfa6f2d025d17336126dd
Cassia Karen de Sousa Santos <cassiakaren56@gmail.com>
2020-11-29 14:31:49 -0300

    Seleçao de elementos 1

---
+++ DOM Para Iniciantes/selecionar o dom/script.js
@@ -0,0 +1,17 @@
+const animais = document.getElementById('animais');
+console.log(animais);
+
+const gridSection = document.getElementsByClassName('grid-section');
+console.log(gridSection[1]);
+
+const primeiraLi = document.querySelector('li');
+console.log(primeiraLi);
+
+const primeiraUl = document.querySelector('ul');
+console.log(primeiraUl);
+
+const linkInterno = document.querySelector('[href^="#"]');
+console.log(linkInterno);
+
+const animaisImgs = document.querySelectorAll('.animais img');
+console.log(animaisImgs[1]);
