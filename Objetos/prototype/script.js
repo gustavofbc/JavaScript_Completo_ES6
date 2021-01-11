7304eefb148a6313165776abac9761b0372218d8
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2021-01-11 16:44:38 -0300

    prototype 2

--- Objetos/prototype/script.js
+++ Objetos/prototype/script.js
@@ -4,9 +4,6 @@ function Pessoa(nome, idade){
     this.abracar = function() {
         return 'Abraçou'
     }
-    this.andar = function() {
-        return 'andou pelo objeto'
-    }
 }
 
 var objeto = {
@@ -26,6 +23,24 @@ Pessoa.prototype.nadar = function() {
 
 const andre = new Pessoa('André', 28);
 
-console.log(Pessoa.prototype)
-console.log(andre.prototype)
+// console.log(Pessoa.prototype)
+// console.log(andre.prototype)
 
+const pais = 'Brasil';
+const cidade = new String('Rio');
+
+const listaAnimais = ['cachorro', 'gato', 'gato', 'cavalo'];
+
+const lista = document.querySelectorAll('li');
+//transformando a nodeList "lista" em em Array
+const listaArray1 = Array.prototype.slice.call(lista);
+//ou
+const listaArray2 = Array.from(lista);
+
+const Carro = {
+    marca: 'Ford',
+    preco: 2000,
+    andar() {
+        return true
+    }
+}
