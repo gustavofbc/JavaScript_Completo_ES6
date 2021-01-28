5dbde22cd43483c4334034c8d392bed2e53d0b0b
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2021-01-28 08:27:30 -0300

    native, host e user 2

--- Objetos/native-host-user/script.js
+++ Objetos/native-host-user/script.js
@@ -1,8 +1,27 @@
-console.log(typeof Array.from !== 'undefined')
+// Liste 5 objetos nativos
+Object
+String
+Number
+Array
+Function
 
-if(typeof Array.from !== "undefined"){
+// Liste 5 objetos do browser
+Window
+Document
+NodeList
+HTMLCollection
+alert
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+if(typeof document.webkitHidden !== 'undefined'){
     console.log('Existe')
+} else {
+    console.log('Não existe')
 }
-else {
+
+if(typeof document.webkitVisibilityState !== 'undefined'){
+    console.log('Existe')
+} else {
     console.log('Não existe')
 }
