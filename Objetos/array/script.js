f87b64be785e944b5ee5d9292be439da322f05c0
gustavofbc <gustavo.felipebc@gmail.com>
2021-06-26 14:09:38 -0300

    array 2

--- Objetos/array/script.js
+++ Objetos/array/script.js
@@ -53,4 +53,34 @@ console.log(retiraPrimeiroValorDaArrray)
 console.log(retiraUltimaValorDaArrray)
 
 let novosCarrosReverso = novosCarros.reverse();
-console.log(novosCarrosReverso)
+console.log(novosCarrosReverso);
+
+console.log(novosCarros);
+novosCarros.splice(0, 1, 'Fusca');
+console.log(novosCarros);
+
+console.log(['item1', 'item2', 'item3', 'item4', 'item5'].copyWithin(2,0,2))
+
+console.log(['item1', 'item2', 'item3', 'item4', 'item5'].fill('banana'));
+
+const transportes1 = ['carro', 'moto'];
+const transportes2 = ['avião', 'barco'];
+
+console.log(transportes1.concat(transportes2))
+
+const linguagens = ['html', 'css', 'js', 'php', 'js']
+
+console.log(linguagens.includes('css'));//verifica se tem o item e retorna um boolean
+console.log(linguagens.indexOf('js'));//verifica e retorna o index do primeiro item buscado no array
+console.log(linguagens.lastIndexOf('js')); //verifica e retorna o index do ultimo item buscado no array
+
+let htmlString = '<h2>Olá mundo!</h2>';
+
+let arrayHtmlString = htmlString.split('h2'); //divide o item em elementos de um array com base no que for passado como argumento da fumção
+arrayHtmlString[1] = '>Olá Gustavo!<';
+let newHtmlString = arrayHtmlString.join('h1');//concatena os items do array e se passar o método vázio ele adiciona "," entre cada item concatenado
+console.log(newHtmlString);
+
+console.log(linguagens.slice(1)); //começa do item que você passou primeiro e vai até o que você passou como segundo
+
+let copiaLinguagens = linguagens.slice();//se não passar argumentos ele copia toda o array
