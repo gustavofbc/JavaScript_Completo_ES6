ef334fea05aa69253d6a3dbd6c3ccbea128c697b
gustavofbc <gustavo.felipebc@gmail.com>
2021-11-19 16:52:53 -0300

    modules 2

--- Efeitos do DOM/modules-introducao/js/script.js
+++ Efeitos do DOM/modules-introducao/js/script.js
@@ -1,10 +1,10 @@
 import initScrollSuave from "./modules/scroll-suave.js";
 import initScrollAnimacao from "./modules/scroll-animacao.js";
-import InnitAcordion from "./modules/accordion.js";
+import InitAcordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js";
 
 //note: a ordem de adição dos módulos importa
 initScrollSuave();
 initScrollAnimacao();
-InnitAcordion();
+InitAcordion();
 initTabNav();
