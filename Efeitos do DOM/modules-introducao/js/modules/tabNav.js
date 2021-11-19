811c71d4c34df99ab02103302a84bcb0965ab32f
gustavofbc <gustavo.felipebc@gmail.com>
2021-11-19 16:36:58 -0300

    modules 1

---
+++ Efeitos do DOM/modules-introducao/js/modules/tabNav.js
@@ -0,0 +1,26 @@
+export default function initTabNav()
+{
+    const tabMenu = document.querySelectorAll('[data-tab="menu"]  li'); //limito que busco os "li" dentro da class
+    const tabContent = document.querySelectorAll('[data-tab="content"] section'); //limito que busco os "section" dentro da class
+
+    //verifico se os itens existem na tela antes, caso não, as funções não devem ocorrer
+    if(tabMenu.length && tabContent.length){
+        //adicionar a classe ativa no primeiro elemento direto
+        tabContent[0].classList.add('ativo');
+
+        function activeTab(index) {
+            tabContent.forEach( (section) => {
+                section.classList.remove('ativo');
+            });
+            const direcao = tabContent[index].dataset.anime;
+
+            tabContent[index].classList.add('ativo', direcao);
+        }
+
+        tabMenu.forEach((itemMenu, index) => {
+            itemMenu.addEventListener('click', () => {
+                activeTab(index);
+            })
+        })
+    }
+}
