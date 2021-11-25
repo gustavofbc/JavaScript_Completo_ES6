ecae33d8266fef69df5fbd9a1c8b906db76886f2
gustavofbc <gustavo.felipebc@gmail.com>
2021-11-25 17:56:05 -0300

    modal 2

--- Efeitos do DOM/modal/js/script.js
+++ Efeitos do DOM/modal/js/script.js
@@ -2,9 +2,11 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initScrollAnimacao from "./modules/scroll-animacao.js";
 import InitAcordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js";
+import initModal from "./modules/modal.js";
 
 //note: a ordem de adição dos módulos importa
 initScrollSuave();
 initScrollAnimacao();
 InitAcordion();
 initTabNav();
+initModal();
