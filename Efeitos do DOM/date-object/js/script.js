d35f608b7694f93f49c7f97e8eb9f7d4d3413747
gustavofbc <gustavo.felipebc@gmail.com>
2022-01-14 17:52:10 -0300

    date object

---
+++ Efeitos do DOM/date-object/js/script.js
@@ -0,0 +1,22 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initScrollAnimacao from "./modules/scroll-animacao.js";
+import InitAcordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabNav.js";
+import initModal from "./modules/modal.js";
+import initTooltip from "./modules/tootip.js";
+import initDropdownMenu from "./modules/dropdown-menu.js";
+import initMenuMobile from "./modules/menu-mobile.js";
+import initAnimaNumeros from "./modules/anima-numeros.js";
+import initFuncionamento from "./modules/funcionamento.js";
+
+//note: a ordem de adição dos módulos importa
+initScrollSuave();
+initScrollAnimacao();
+InitAcordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropdownMenu();
+initMenuMobile();
+initAnimaNumeros();
+initFuncionamento();
