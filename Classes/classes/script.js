73a2af828a3d217ca5be608cdb92101770dcff86
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-02-09 17:53:21 -0300

    get e set

--- Classes/classes/script.js
+++ Classes/classes/script.js
@@ -1,102 +1,37 @@
-// # CRIANDO COM CONSTRUCTOR FUNCTION
-
-// function Button(text, background){
-//     this.text = text;
-//     this.background = background;
-// }
-
-// Button.prototype.element = function() {
-//     const buttonElement = document.createElement('button');
-//     buttonElement.innerText = this.text;
-//     buttonElement.style.background = this.background;
-//     return buttonElement;
-// }
-
-// const blueButton = new Button('Comprar', 'blue');
-
-// console.log(blueButton.element())
-
-// =================================================================
-
-// # CRIANDO COM CLASS 
-
-// class Button {
-//     constructor(text, background, color) {
-//         this.text = text;
-//         this.background = background;
-//         this.color = color;
-//     }
-//     element() {
-//         const buttonElement = document.createElement('button');
-//         buttonElement.innerText = this.text;
-//         buttonElement.style.background = this.background;
-//         buttonElement.style.color = this.color;
-//         return buttonElement;
-//     }
-//     appendTo(target) {
-//         const targetElement = document.querySelector(target);
-//         targetElement.appendChild(this.element());
-//         console.log(this);
-//         return targetElement;
-//     }
-// }
-
-// const blueButton = new Button('Comprar', 'blue', 'white');
-
-// console.log(blueButton.appendTo('body'))
-
-
-// =================================================================
-
-// # STATIC
-
-// class Button {
-//     constructor(options) {
-//         this.options = options
-//     }
-//     static createButton(text, background) {
-//         const buttonElement = document.createElement('button');
-//         buttonElement.innerText = text;
-//         buttonElement.style.background = background;
-//         return buttonElement;
-//     }
-// }
-
-// const optionsBlue  = {
-//     backgroundColor: 'blue',
-//     text: 'comprar',
-//     color: 'white'
-// }
-
-// const blueButton = new Button(optionsBlue);
-
-// const blueButtonStatic = Button.createButton('clique', 'red');
-
-
-class Button {
-    constructor(text, background, color) {
-        this.text = text;
-        this.background = background;
-        this.color = color;
-    }
-    element() {
-        const buttonElement = document.createElement('button');
-        buttonElement.innerText = this.text;
-        buttonElement.style.background = this.background;
-        buttonElement.style.color = this.color;
-        return buttonElement;
-    }
-    appendTo(target) {
-        const targetElement = document.querySelector(target);
-        targetElement.appendChild(this.element());
-        return targetElement;
-    }
-    static blueButton(text) {
-        return new Button(text, 'background', 'white')
+const button = {
+    get tamanho() {
+        return this._numero || 100;
+    },
+    set tamanho(numero) {
+        this._numero = numero * 100;
     }
 }
 
-const botaoAzul = Button.blueButton('Teste');
+const frutas = {
+    lista: [],
+    set nova(fruta) {
+        this.lista.push(fruta);
+    }
+}
 
+frutas.nova = 'banana';
+frutas.nova = 'morango';
 
-// console.log(blueButton.appendTo('body'))
+class Button {
+    constructor(text, color) {
+        this.text = text;
+        this.color = color;
+    }
+    get element() {
+        const type = this._elementType || 'button';
+        const buttonElement = document.createElement(type);
+        buttonElement.innerText = this.text;
+        buttonElement.style.color = this.color;
+        return buttonElement;
+    }
+    set element(type) {
+        this._elementType = type;
+    }
+}
+
+const blueButton = new Button('Comprar', 'blue');
