c2fe3e76f7a609dd2a9bffbb40172561c6c5b4fa
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-02-25 17:51:17 -0300

    regexp metodos 2

--- Regular expression/regexp-metodos/script.js
+++ Regular expression/regexp-metodos/script.js
@@ -17,12 +17,48 @@
 // }
 
 
-const frase = 'JavaScript, TypeScript, CoffeeScript, Java';
+// const frase = 'JavaScript, TypeScript, CoffeeScript, Java, 200';
 
-const regexp = /\w+/g;
+// const regexp = /[A-Za-z]/g;
 
-let i = 1;
-let regexResult;
-while( (regexResult = regexp.exec(frase)) !== null ){
-    console.log(regexResult[0]);
-}
+// let i = 1;
+// let regexResult;
+// while( (regexResult = regexp.exec(frase)) !== null ){
+//     console.log(regexResult[0]);
+// }
+
+// const resultados = frase.split(regexp)
+
+// console.log(resultados)
+
+// const tags = `
+// <ul>
+//     <li>Item 1</li>
+//     <li>Item 2</li>
+// </ul>
+// `
+
+// const regexp = /<li/g;
+
+// const resultado = tags.replace(regexp, '$& class="ativa"');
+// console.log(resultado)
+
+const emails = `joao@homail.com.br
+marta@ggmail.com.br
+bruna@oulook.com.br
+andre@origamid.com`
+
+const regexp = /(\w+@)([\w.]+)/g;
+const resultado = emails.replace(regexp, function(...args) {
+    if(args[2] == 'homail.com.br'){
+        return args[1]+'hotmail.com.br'
+    } else if(args[2] == 'ggmail.com.br'){
+        return args[1]+'gmail.com.br'
+    } else if(args[2] == 'oulook.com.br'){
+        return args[1]+'outlook.com.br'
+    } else {
+        return args[0]
+    }
+});
+
+console.log(resultado)
