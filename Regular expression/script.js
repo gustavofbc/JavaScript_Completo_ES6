bf6754238d65fb0363ed4c6ef9cd7341bd9053ca
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-02-25 16:48:14 -0300

    regexp padroes 2

--- Regular expression/script.js
+++ Regular expression/script.js
@@ -48,3 +48,65 @@ const cnpjs = [
 for(cnpj of cnpjs){
     console.log(cnpj, cnpj.match(regexcnpj))
 }
+
+// minha solução
+const regexTelefone = /(?:[+(]?\d{2}[ )]?)?(\d{2}?[\s-]?)?\d{4,5}[ -]?\d{4}/g;
+const telefones = [
+    '+55 11 98888-8888',
+    '+55 11 98888 8888',
+    '+55 11 988888888',
+    '+55 11988888888',
+    '+5511988888888',
+    '5511988888888',
+    '11-98888-8888',
+    '11 98888 8888',
+    '11988888888',
+    '988888888',
+    '(11)988888888',
+    '98888 8888',
+    '8888 8888',
+]
+
+for(telefone of telefones){
+    console.log(telefone, telefone.match(regexTelefone))
+}
+
+
+const regexEmails = /[\w.-]+@[\w-]+\.[\w .]+/gi;
+const emails = [
+    'email@email.com',
+    'email@email.com.org',
+    'email-email@email.com',
+    'email_email@email.com',
+    'email.email23@email.com.br',
+    'email.email23@empresa-sua.com.br',
+    'c@contato.cc',
+]
+
+for(email of emails){
+    console.log(email, email.match(regexEmails))
+}
+
+
+const regexTag = /<\/?[\w\s="']+\/?>?/gi
+const tags = [
+    '<div>Isso é uma div</div>',
+    '<div class="ativa">Essa está ativa</div>',
+    '<img src="imagem" />',
+    '<img src="imagem">',
+    '<ul class="ativa">',
+    '<li>Essa está ativa</li>',
+    '</ul>'
+  ];
+
+const regexNomeTags = /(?<=<\/?)[\w]+/gi;
+const nomeTags = [
+    '<div>Isso é uma div</div>',
+    '<div class="ativa">Essa está ativa</div>',
+    '<img src="imagem" />',
+    '<img src="imagem">',
+    '<ul class="ativa">',
+    '<li>Essa está ativa</li>',
+    '</ul>'
+];
+
