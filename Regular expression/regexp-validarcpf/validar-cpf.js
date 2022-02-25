4422e936587272d5daa036bbc9aba0edc43ae865
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-02-25 19:50:59 -0300

    validar CPF 1

---
+++ Regular expression/regexp-validarcpf/validar-cpf.js
@@ -0,0 +1,17 @@
+export default class ValidarCpf {
+    constructor(element) {
+        this.element = element
+    }
+
+    limpar(cpf){
+        return cpf.replace(/\D/g, '')
+    }
+    construir(cpf){
+        return cpf.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/g, '$1.$2.$3-$4')
+    }
+    formatar(cpf){
+        const cpfLimpo = this.limpar(cpf);
+        return this.construir(cpfLimpo);
+    }
+}
+
