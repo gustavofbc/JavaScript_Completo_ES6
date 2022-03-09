8d8c32406f654b3eabc864e10eda13b1fc369867
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-03-09 20:47:59 -0300

    accordion refatorado para classe

--- Projeto-final/projeto-final/js/script.js
+++ Projeto-final/projeto-final/js/script.js
@@ -1,5 +1,5 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import InitAcordion from "./modules/accordion.js";
+import Accordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tootip.js";
@@ -10,18 +10,12 @@ import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 import initScrollAnimacao from "./modules/scroll-animacao.js";
 
-const options = {
-  behavior: "smooth",
-  block: "start",
-};
-const scrollSuave = new ScrollSuave(
-  '[data-menu="suave"] a[href^="#"]',
-  options
-);
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 //note: a ordem de adição dos módulos importa
-InitAcordion();
 initTabNav();
 initModal();
 initTooltip();
