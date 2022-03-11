32b8a14e0c74e9ce9394c99b362f8fe68e20e895
Gustavo Felipe <gustavo.felipebc@gmail.com>
2022-03-11 17:16:50 -0300

    modal refatorado com class

--- Projeto-final/projeto-final/js/script.js
+++ Projeto-final/projeto-final/js/script.js
@@ -1,7 +1,7 @@
 import ScrollSuave from "./modules/scroll-suave.js";
 import Accordion from "./modules/accordion.js";
 import TabNav from "./modules/tabNav.js";
-import initModal from "./modules/modal.js";
+import Modal from "./modules/modal.js";
 import initTooltip from "./modules/tootip.js";
 import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
@@ -22,8 +22,14 @@ const tabNav = new TabNav(
 );
 tabNav.init();
 
+const modal = new Modal(
+  '[data-modal="abrir"]',
+  '[data-modal="fechar"]',
+  '[data-modal="container"]'
+);
+modal.init();
+
 //note: a ordem de adição dos módulos importa
-initModal();
 initTooltip();
 initDropdownMenu();
 initMenuMobile();
